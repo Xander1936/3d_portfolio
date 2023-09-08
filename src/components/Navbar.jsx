bab0bc1b68a6dc0b601ebf8da0997ce62d10f625
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-09-08 16:16:13 +0100

    Hero, About sections + Higher Order function: hoc(Wrapper: SectionWrapper.jsx) for css + Some CSS fixs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
         className="w-9 h-9 object-contain" />
         <p className='text-white text-[18px] font-bold cursor-pointer flex'>
           Alexandre Massoda &nbsp; {/* &nbsp veut dire empty space */}
-          <span className='sm:block hidden'>|  Software Developer: ReactJS</span>
+          <span className='sm:block hidden'>|&nbsp; Software Developer - React</span>
         </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -83,7 +83,7 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
+      </div> 
     </nav>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+// Before don't forget to run on the terminal: npm install three;
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-const Computers = () => {
+import CanvasLoader from '../Loader';
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+  
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+      <pointLight intensity={75} />
+      <spotLight 
+        position = {[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object = {computer.scene}
+        scale = {isMobile ? 0.55 : 0.7}
+        position = {isMobile ? [-3, -2, -2.2] : [0, -2.6, -1.5]}
+        rotation = {[-0.01, -0.1, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a Listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    // Add the callback function as a Listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Remove the Listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', 
+      handleMediaQueryChange);
+    }
+  }, [])
+  
+
+  return (
+    <Canvas
+      className='xs: my-4'
+      frameLoop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false} 
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
+// <></>
